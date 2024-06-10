1dac80db27f7c05822a291fab9e0db494e4d1a93
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2024-06-10 12:38:25 -0300

    initial commit

---
+++ src/main/java/com/lgs/MathController.java
@@ -0,0 +1,42 @@
+package com.lgs;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lgs.exceptions.UnsupportedMathOperationException;
+
+@RestController
+public class MathController {
+
+	private static final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double sum(@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value");
+		}
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+
+	private Double convertToDouble(String StrNumber) {
+		if (StrNumber == null) {
+			return 0D;
+		}
+		String number = StrNumber.replaceAll(",", ".");
+		if (isNumeric(number))
+			return Double.parseDouble(number);
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null)
+			return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+}
---
+++ src/test/java/com/example/demo/Projecto1JavAcursoAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projecto1JavAcursoAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
