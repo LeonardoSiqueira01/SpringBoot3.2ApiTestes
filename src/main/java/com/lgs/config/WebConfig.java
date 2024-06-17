4076b29a304af9235bc2d2c513b576254a7f3fb9
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2024-06-17 10:25:42 -0300

    1304 Content Negotiation via HEADER Parameter

--- src/main/java/com/lgs/config/WebConfig.java
+++ src/main/java/com/lgs/config/WebConfig.java
@@ -8,12 +8,27 @@ import org.springframework.http.MediaType;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
+	/*@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-
+ //http://localhost:8081/person/v1?mediaType=xml
+  // http://localhost:8081/person/v1/7?mediaType=xml
+ //CONFIGURANDO VIA QUERY PARAM.
+  
 		configurer.favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true)
 				.useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
 				.mediaType("json", MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML);
+	}*/
+	
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		//CONFIGURANDO VIA HEADER PARAM.
+		configurer.favorParameter(false).ignoreAcceptHeader(false)
+		.useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
+		.mediaType("json", MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML);
+	//Exemplos de chamadas
+		 //http://localhost:8081/person/v1?mediaType=xml
+		  //http://localhost:8081/person/v1/7?mediaType=xml
 	}
 
 }
